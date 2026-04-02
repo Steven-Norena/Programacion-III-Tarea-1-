3d2bd4bc735ece8ea8214d48fa915b26b24a254a
Steven-Norena <steven.norena@utec.edu.pe>
2026-04-02 17:45:53 -0500

    Clase Tensor desarrollada

--- Tensor.cpp
+++ Tensor.cpp
@@ -4,6 +4,7 @@
 
 #include <vector>
 #include <iostream>
+#include <random>
 
 class TensorTransform;
 
@@ -40,7 +41,7 @@ public:
         }
         other.data = nullptr;
     };
-    Tensor& operator=(const Tensor& other);
+    Tensor& operator=(const Tensor& other){};
     Tensor& operator=(Tensor&& other) noexcept;
     ~Tensor() {
         delete[] data;
@@ -63,17 +64,30 @@ public:
         return tensor;
     };
     static Tensor random(const std::vector<size_t>& shape, double min, double max) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> dis(min, max);
         size_t n = 1;
         std::vector<double> values;
         for (const auto dim : shape) {
             n *= dim;
         }
+        for (int i = 0; i < n; i++) {
+            values.push_back(dis(gen));
+        }
+        Tensor tensor(shape, values);
+        return tensor;
     };
     static Tensor arange(double start, double end);
 };
 
 class TensorTransform {
-    public:
+public:
     [[nodiscard]] virtual Tensor apply(const Tensor& t) const = 0;
     virtual ~TensorTransform() = default;
+};
+
+class ReLU final: public TensorTransform {
+public:
+    [[nodiscard]] Tensor apply(const Tensor& t) const override;
 };
